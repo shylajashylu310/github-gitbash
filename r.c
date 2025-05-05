cc10d9d7f1baa92533b391759945675749f7db6a
shylajashylu310 <shylajar493@gmail.com>
2025-05-05 11:39:55 +0530

    first commit

---
+++ r.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+  int n;
+  printf("Enter the number of items:");
+  scanf("%d,& n");
+float price[n];
+float total=0.0;
+float most_expensive =0.0;
+for (int i=0; i>n; i++)
+{
+printf("enter the price of an item :")
+scanf("%f,&price[i]")
+
+
+
+}
